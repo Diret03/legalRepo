fed0dc59bec0a92dc110ccba07fdd5313a32b537
Diret03 <diegodavidrecalde@gmail.com>
2024-10-09 11:56:50 -0500

    responsive navbar elements

--- resources/views/cases/partials/all-list.blade.php
+++ resources/views/cases/partials/all-list.blade.php
@@ -1,5 +1,6 @@
 @foreach ($cases as $case)
-    <div class="py-6 px-10 bg-white border border-gray-200 rounded-lg shadow mb-4 transform transition-transform duration-300 hover:translate-x-2 hover:-translate-y-2 hover:shadow-lg">
+    <div
+        class="py-6 px-10 bg-white border border-gray-200 rounded-lg shadow mb-4 transform transition-transform duration-300 hover:translate-x-2 hover:-translate-y-2 hover:shadow-lg active:translate-x-2 active:-translate-y-2 active:shadow-lg">
 
         <a href="{{ route('cases.show', $case->id) }}">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 underline">{{ $case->title }}</h5>
@@ -22,12 +23,12 @@
         </div>
 
         <a href="{{ route('cases.show', $case->id) }}"
-           class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-stone-700 rounded-lg hover:bg-stone-400 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300">
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-stone-700 rounded-lg hover:bg-stone-400 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300">
             Ver más
             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                 fill="none" viewBox="0 0 14 10">
+                fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    d="M1 5h12m0 0L9 1m4 4L9 9" />
             </svg>
         </a>
     </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,7 +7,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800"/>
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
@@ -31,9 +31,7 @@
                             }
                         @endphp
                         <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <x-nav-link
-                                :href="$dashboardLink"
-                                :active="request()->routeIs('dashboard') || request()->is('dashboard/*')">
+                            <x-nav-link :href="$dashboardLink" :active="request()->routeIs('dashboard') || request()->is('dashboard/*')">
                                 {{ __('Dashboard') }}
                             </x-nav-link>
                         </div>
@@ -70,10 +68,10 @@
 
                                     <div class="ms-1">
                                         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                             viewBox="0 0 20 20">
+                                            viewBox="0 0 20 20">
                                             <path fill-rule="evenodd"
-                                                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                  clip-rule="evenodd"/>
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
                                         </svg>
                                     </div>
                                 </button>
@@ -89,7 +87,7 @@
                                     @csrf
 
                                     <x-dropdown-link :href="route('logout')"
-                                                     onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                         {{ __('Cerrar sesión') }}
                                     </x-dropdown-link>
@@ -100,11 +98,11 @@
                 @else
                     <div class="hidden sm:flex sm:items-center sm:ms-6 gap-3">
                         <a href="{{ route('login') }}"
-                           class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none ">
+                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none ">
                             Iniciar sesión
                         </a>
                         <a href="{{ route('register') }}"
-                           class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none ">
+                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none ">
                             Registrarse
                         </a>
                     </div>
@@ -116,13 +114,13 @@
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open"
-                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
-                              stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M4 6h16M4 12h16M4 18h16"/>
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
-                              stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -130,10 +128,21 @@
     </div>
 
     @if (Route::has('login'))
-
         <!-- Responsive Navigation Menu -->
-        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
             <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                    {{ __('Inicio') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('subjects.list')" :active="request()->routeIs('subjects.list') || request()->is('materias/*')">
+                    {{ __('Materias') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('tags.list')" :active="request()->routeIs('tags.list') || request()->is('etiquetas/*')">
+                    {{ __('Etiquetas') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('cases.list')" :active="request()->routeIs('cases.list') || request()->is('casos/*')">
+                    {{ __('Casos') }}
+                </x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
                 </x-responsive-nav-link>
@@ -156,15 +165,14 @@
                             @csrf
 
                             <x-responsive-nav-link :href="route('logout')"
-                                                   onclick="event.preventDefault();
+                                onclick="event.preventDefault();
                                         this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-responsive-nav-link>
                         </form>
                     </div>
                 </div>
-        </div>
-         @endauth
+            </div>
+        @endauth
     @endif
 </nav>
-
