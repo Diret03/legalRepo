c32baee77fbbc7276241170d148f4e7c142c6b86
Diret03 <diegodavidrecalde@gmail.com>
2024-10-19 12:14:09 -0500

    updated seeders

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -25,10 +25,10 @@ class SubjectSeeder extends Seeder
             'description' => 'Asuntos sobre derechos y obligaciones laborales, contratos, despidos y relaciones de trabajo.',
         ]);
 
-        Subject::create([
-            'name' => 'Inquilinato',
-            'description' => 'Casos vinculados a arrendamientos de inmuebles, derechos de inquilinos y propietarios.',
-        ]);
+//        Subject::create([
+//            'name' => 'Inquilinato',
+//            'description' => 'Casos vinculados a arrendamientos de inmuebles, derechos de inquilinos y propietarios.',
+//        ]);
 
         Subject::create([
             'name' => 'Civil',
@@ -40,9 +40,19 @@ class SubjectSeeder extends Seeder
             'description' => 'Delitos y crímenes que afectan el orden público y que son sancionados por la ley penal.',
         ]);
 
+//        Subject::create([
+//            'name' => 'Tributario',
+//            'description' => 'Casos relacionados con la recaudación de impuestos, derechos fiscales y obligaciones tributarias.',
+//        ]);
+
         Subject::create([
-            'name' => 'Tributario',
-            'description' => 'Casos relacionados con la recaudación de impuestos, derechos fiscales y obligaciones tributarias.',
+            'name' => 'Procesal Código Orgánico Integral Penal',
+            'description' => 'Procedimientos judiciales en el ámbito penal, conforme al Código Orgánico Integral Penal (COIP), que regula los delitos, las sanciones y el proceso para juzgarlos.',
+        ]);
+
+        Subject::create([
+            'name' => 'Procesal Código Orgánico General de Procesos',
+            'description' => 'Procedimientos judiciales en materia civil, mercantil, laboral y de familia, basados en el Código Orgánico General de Procesos (COGEP), que establece las reglas procesales para resolver conflictos de manera ordenada.',
         ]);
     }
 }
--- database/seeders/TrialSeeder.php
+++ database/seeders/TrialSeeder.php
@@ -65,67 +65,92 @@ class TrialSeeder extends Seeder
         ]);
 
         // Inquilinato
-        Trial::create([
-            'subject_id' => 3,
-            'name' => 'Desalojo por Falta de Pago',
-            'description' => 'Proceso judicial para solicitar el desalojo del inquilino por falta de pago de la renta.'
-        ]);
-
-        Trial::create([
-            'subject_id' => 3,
-            'name' => 'Reparación de Daños a la Propiedad',
-            'description' => 'Juicio en el que el propietario demanda al inquilino por daños a la propiedad durante el arrendamiento.'
-        ]);
+//        Trial::create([
+//            'subject_id' => 3,
+//            'name' => 'Desalojo por Falta de Pago',
+//            'description' => 'Proceso judicial para solicitar el desalojo del inquilino por falta de pago de la renta.'
+//        ]);
+//
+//        Trial::create([
+//            'subject_id' => 3,
+//            'name' => 'Reparación de Daños a la Propiedad',
+//            'description' => 'Juicio en el que el propietario demanda al inquilino por daños a la propiedad durante el arrendamiento.'
+//        ]);
 
         // Civil
         Trial::create([
-            'subject_id' => 4,
+            'subject_id' => 3,
             'name' => 'Deuda Lícita',
             'description' => 'Juicio para la recuperación de deudas lícitas.'
         ]);
 
         Trial::create([
-            'subject_id' => 4,
+            'subject_id' => 3,
             'name' => 'Alimentos Congruos y Necesarios',
             'description' => 'Juicio por alimentos congruos y necesarios.'
         ]);
 
         Trial::create([
-            'subject_id' => 4,
+            'subject_id' => 3,
             'name' => 'Incumplimiento de contratos',
             'description' => 'Juicio por incumplimiento de contratos.'
         ]);
 
         Trial::create([
-            'subject_id' => 4,
+            'subject_id' => 3,
             'name' => 'Contrato de servicios profesionales',
             'description' => 'Juicio por problemas en contratos de servicios profesionales.'
         ]);
 
         Trial::create([
-            'subject_id' => 4,
+            'subject_id' => 3,
             'name' => 'Conflictos vecinales',
             'description' => 'Juicios relacionados con conflictos entre vecinos.'
         ]);
 
         // Penal
         Trial::create([
-            'subject_id' => 5,
+            'subject_id' => 4,
             'name' => 'Tránsito Daños Materiales',
             'description' => 'Juicio por daños materiales en accidentes de tránsito.'
         ]);
 
         Trial::create([
-            'subject_id' => 5,
+            'subject_id' => 4,
             'name' => 'Tránsito Lesiones',
             'description' => 'Juicio por lesiones en accidentes de tránsito.'
         ]);
 
         // Tributario
+//        Trial::create([
+//            'subject_id' => 6,
+//            'name' => 'SRI: multas recargos e intereses',
+//            'description' => 'Juicio tributario sobre multas, recargos e intereses del SRI, con el 25% de la deuda cancelada.'
+//        ]);
+
+        //Procesal Código Orgánico Integral Penal
+        Trial::create([
+            'subject_id' => 5,
+            'name' => 'Delitos Contra La Vida',
+            'description' => 'Juicios relacionados con delitos que atentan contra la vida, de acuerdo con el Código Orgánico Integral Penal.',
+        ]);
+
+        Trial::create([
+            'subject_id' => 5,
+            'name' => 'Delitos Contra La Integridad Física',
+            'description' => 'Juicios relacionados con delitos que afectan la integridad física de las personas bajo el Código Orgánico Integral Penal.',
+        ]);
+        //Procesal Código Orgánico General de Procesos
         Trial::create([
             'subject_id' => 6,
-            'name' => 'SRI: multas recargos e intereses',
-            'description' => 'Juicio tributario sobre multas, recargos e intereses del SRI, con el 25% de la deuda cancelada.'
+            'name' => 'Procesos De Insolvencia',
+            'description' => 'Juicios relacionados con insolvencia y quiebra bajo el Código Orgánico General de Procesos.',
+        ]);
+
+        Trial::create([
+            'subject_id' => 6,
+            'name' => 'Procesos De Familia',
+            'description' => 'Juicios relacionados con el derecho de familia bajo el Código Orgánico General de Procesos.',
         ]);
     }
 }
