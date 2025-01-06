fd7e3aa596fe3da4abce56f656a2f40107689527
Diret03 <diegodavidrecalde@gmail.com>
2025-01-06 08:45:05 -0500

    default option in trials edit form

--- resources/views/trials/row.blade.php
+++ resources/views/trials/row.blade.php
@@ -74,7 +74,11 @@
                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Materia</label>
                                                 <select name="subject_id" id="edit-subject_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                                                     @foreach($subjects as $subject)
-                                                        <option value="{{$subject->id}}">{{$subject->name}}</option>
+                                                        @if ($subject->id === $trial->subject->id)
+                                                            <option selected value="{{$subject->id}}">{{$subject->name}}</option>
+                                                        @else
+                                                            <option value="{{$subject->id}}">{{$subject->name}}</option>
+                                                        @endif
                                                     @endforeach
                                                 </select>
                                             </div>
